6bdd1ee6f7349f7f549ea81516ec4e01eee3fc5a
Bruno Mendes Augusto <brunomendesaugusto@gmail.com>
2025-04-11 11:09:53 +0000

    Upload de Notes file and main.tf fix

--- main.tf
+++ main.tf
@@ -1,4 +1,25 @@
-provider "aws" {
-  region = "us-east-1"
+terraform {
+  required_version = "0.14.4"
+  requied_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.23.0"
+    }
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+resource "aws_s3_bucket" "bruno_bucket" {
+  bucket = "bruno-maestriatec-terraform-iac-001"  # Precisa ser único no mundo
+  force_destroy = true
+
+  tags = {
+    Projeto   = "Curso Terraform"
+    Ambiente  = "Dev"
+    CriadoPor = "Bruno"
+  }
+}
